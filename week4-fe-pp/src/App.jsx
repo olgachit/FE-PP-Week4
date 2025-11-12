c903f171c98867a7a97a224ee504d32b77fd5bd2
Olga <olga.chitembo@metropolia.fi>
2025-11-12 14:15:29 +0200

    fixed importing

--- week4-fe-pp/src/App.jsx
+++ week4-fe-pp/src/App.jsx
@@ -4,8 +4,10 @@ import Hero from "./components/Hero";
 import Header from "./components/Header";
 import Services from "./components/Services";
 import Tours from "./components/Tours";
+import Home from "./components/Home";
+import NotFound from "./components/NotFound";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
